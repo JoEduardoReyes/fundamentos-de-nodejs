8c4dafd1b846482a9f773d7a3cfe90769e66b82c
Eduardo Reyes <joeduardoreyes@outlook.com>
2021-10-06 11:23:22 -0600

    Nodemon y PM2 hacen mucho mas facil la vida

--- conceptos/monohilo.js
+++ conceptos/monohilo.js
@@ -6,9 +6,9 @@ setInterval(() => {
   console.log(i);
   i++;
 
-  if (i === 5) {
-    var a = 3 + z;
-  }
+  // if (i === 5) {
+  //   var a = 3 + z;
+  // }
 }, 1000);
 
 console.log("Segundo instruccion");
